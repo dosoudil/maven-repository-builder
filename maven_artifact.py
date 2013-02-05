4045a97261b308fd7bb114993c624829851ddeeb
Paul Gier <pgier@redhat.com>
2013-02-04 19:18:06 -0600

    Refactoring to clean up code

--- maven_artifact.py
+++ maven_artifact.py
@@ -3,7 +3,9 @@
 class MavenArtifact:
 
     def __init__(self, gav):
-        """Initialize an artifact using a colon separated GAV to relative path groupId:artifactId:type:version"""
+        """Initialize an artifact using a colon separated 
+           GAV to relative path groupId:artifactId:type:version
+        """
         gavParts = gav.split(':')
         if (len(gavParts) > 3):
             self.groupId = gavParts[0]
@@ -13,32 +15,39 @@ class MavenArtifact:
         else: 
             print 'Invalid GAV string: ' + gav
 
-
-    def getRelativePath(self):
+    def getDirPath(self):
         """Get the relative repository path to the artifact"""
         relativePath = self.groupId.replace('.', '/') + '/'
         relativePath += self.artifactId + '/'
         relativePath += self.version + '/'
         return relativePath
 
-
     def getBaseFilename(self):
         """Returns the filename without the file extension"""
         filename = self.artifactId + '-' + self.version 
         return filename
 
-
     def getArtifactFilename(self):
         """Returns the filename of the artifact"""
         return self.getBaseFilename() + '.' + self.artifactType
 
+    def getArtifactFilepath(self):
+        """Return the path to the artifact file"""
+        return self.getDirPath() + '/' + self.getArtifactFilename()
 
     def getPomFilename(self):
         """Returns the filename of the pom file for this artifact"""
         return self.getBaseFilename() + '.pom'  
 
+    def getPomFilepath(self):
+        """Return the path to the artifact file"""
+        return self.getDirPath() + '/' + self.getPomFilename()
 
     def getSourcesFilename(self):
         """Returns the filename of the sources artifact"""
         return self.getBaseFilename() + '-sources.jar'
+
+    def getSourcesFilepath(self):
+        """Return the path to the artifact file"""
+        return self.getDirPath() + '/' + self.getSourcesFilename()
 
