6104a1bf5f5cafca4e493f82f1b2ebe801c2f4a6
Paul Gier <pgier@redhat.com>
2013-05-31 12:14:04 -0500

    Sync maven_artifact.py with other repo builder

--- maven_artifact.py
+++ maven_artifact.py
@@ -5,14 +5,14 @@ import logging
 import re
 import sys
 
-
 class MavenArtifact:
 
-    def __init__(self, groupId, artifactId, type, version):
+    def __init__(self, groupId, artifactId, type, version, classifier=''):
         self.groupId = groupId
         self.artifactId = artifactId
-        self.type = type
+        self.artifactType = type
         self.version = version
+        self.classifier = classifier
 
     @staticmethod
     def createFromGAV(gav):
@@ -23,13 +23,24 @@ class MavenArtifact:
         :returns: MavenArtifact instance
         """
         regexGAV =\
-            re.compile('([\w._-]+):([\w._-]+):(?:([\w._-]+)?:)?([\w._-]+:)?([\d][\w._-]*)(:[\w._-]+)?')
+            re.compile('([\w._-]+):([\w._-]+):(?:([\w._-]+)?:)?(?:([\w._-]+)?:)?([\d][\w._-]*)(:[\w._-]+)?')
         gavParts = regexGAV.search(gav)
         if gavParts is None:
             logging.error("Invalid GAV string: %s", gav)
             sys.exit(1)
-        return MavenArtifact(gavParts.group(1), gavParts.group(2), gavParts.group(3), gavParts.group(5))
+        if (gavParts.group(4)):
+            classifier = gavParts.group(4)
+        else:
+            classifier = ''
 
+        return MavenArtifact(gavParts.group(1), gavParts.group(2), gavParts.group(3), gavParts.group(5), classifier)
+
+    def getArtifactType(self):
+        return self.artifactType
+
+    def getClassifier(self):
+        return self.classifier
+ 
     def getDirPath(self):
         """Get the relative repository path to the artifact"""
         relativePath = self.groupId.replace('.', '/') + '/'
@@ -38,7 +49,7 @@ class MavenArtifact:
         return relativePath
 
     def getGA(self):
-        return self.groupId + ":" + self.artifactId + ":" + self.type
+        return self.groupId + ":" + self.artifactId + ":" + self.artifactType
 
     def getGAV(self):
         return self.groupId + ":" + self.artifactId + ":" + self.version
@@ -48,13 +59,24 @@ class MavenArtifact:
         baseFilename = self.artifactId + '-' + self.version
         return baseFilename
 
+    def getArtifactFilename(self):
+        """Returns the filename of the artifact"""
+        filename = self.getBaseFilename()
+        if (self.classifier):
+            filename += '-' + self.classifier
+        return filename + '.' + self.artifactType
+
+    def getArtifactFilepath(self):
+        """Return the path to the artifact file"""
+        return self.getDirPath() + self.getArtifactFilename()
+
     def getPomFilename(self):
         """Returns the filename of the pom file for this artifact"""
         return self.getBaseFilename() + '.pom'
 
     def getPomFilepath(self):
         """Return the path to the artifact file"""
-        return self.getDirPath() + '/' + self.getPomFilename()
+        return self.getDirPath() + self.getPomFilename()
 
     def getSourcesFilename(self):
         """Returns the filename of the sources artifact"""
@@ -62,11 +84,11 @@ class MavenArtifact:
 
     def getSourcesFilepath(self):
         """Return the path to the artifact file"""
-        return self.getDirPath() + '/' + self.getSourcesFilename()
+        return self.getDirPath() + self.getSourcesFilename()
 
     def __str__(self):
         result = self.groupId + ':' + self.artifactId
-        if self.type:
-            result += ':' + self.type
+        if self.artifactType:
+            result += ':' + self.artifactType
         result += ':' + self.version
         return result
