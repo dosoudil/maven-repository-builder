3ce462978aa2b92858ece2ecb44dff94322d1426
Honza Brázdil <jbrazdil@redhat.com>
2013-06-10 10:49:18 +0200

    Allow usage of artifact_list_generator.py as a separate tool. Close #10

--- artifact_list_generator.py
+++ artifact_list_generator.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 
 import logging
+import optparse
 from configuration import Configuration
 from artifact_list_builder import ArtifactListBuilder
 from filter import Filter
 from maven_artifact import MavenArtifact
+import mrbutils
 
 
-def generateArtifactList(options):
-    """
-    Generates artifact "list" from sources defined in the given configuration in options. The result
-    is dictionary with following structure:
-
-    <repo url> (string)
-      L artifacts (list of MavenArtifact)
-    """
+def _generateArtifactList(options):
     # load configuration
     logging.info("Loading configuration...")
     config = Configuration()
@@ -32,7 +27,18 @@ def generateArtifactList(options):
     artifactList = listFilter.filter(artifactList)
 
     logging.info("Artifact list generation done")
+    return artifactList
 
+def generateArtifactList(options):
+    """
+    Generates artifact "list" from sources defined in the given configuration in options. The result
+    is dictionary with following structure:
+
+    <repo url> (string)
+      L artifacts (list of MavenArtifact)
+    """
+
+    artifactList = _generateArtifactList(options)
     #build sane structure - url to MavenArtifact list
     urlToMAList = {}
     for gat in artifactList:
@@ -50,9 +56,15 @@ def main():
     # Set up logging
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-    generateArtifactList()
+    description = "Generate artifact list from sources defined in the given congiguration file"
+    cliOptParser = optparse.OptionParser(usage="Usage: %prog -c CONFIG", description=description)
+    cliOptParser.add_option('-c', '--config', dest='config',
+            help='Configuration file to use for generation of an artifact list for the repository builder')
+    (options, args) = cliOptParser.parse_args()
 
-    # TODO: pkocandr - process the output and hand it over somehow
+    artifactList = _generateArtifactList(options)
+
+    mrbutils.printArtifactList(artifactList)
 
 
 if __name__ == '__main__':
