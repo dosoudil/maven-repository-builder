b1c4d12e950a458a6f5e8d7915a63146c57629df
Petr Kocandrle <pkocandr@redhat.com>
2013-07-03 09:37:19 +0200

    Fix compatibility of VersionSorter with java 1.6

--- versionSorter/src/main/java/org/jboss/versionsorter/App.java
+++ versionSorter/src/main/java/org/jboss/versionsorter/App.java
@@ -6,19 +6,19 @@ import org.apache.maven.graph.common.version.VersionSpec;
 import org.apache.maven.graph.common.version.VersionUtils;
 
 /**
- * Hello world!
- *
+ * Main class that allows to run the version sorting from command-line.
  */
-public class App 
-{
-    public static void main( String[] args ){
-		ArrayList<VersionSpec> versions = new ArrayList<>();
-		for(String arg : args){
-			versions.add(VersionUtils.createFromSpec(arg));
-		}
-		Collections.sort(versions);
-		for(VersionSpec version : versions){
-			System.out.println(version.renderStandard());
-		}
+public class App {
+
+    public static void main(String[] args) {
+        ArrayList<VersionSpec> versions = new ArrayList<VersionSpec>();
+        for (String arg : args) {
+            versions.add(VersionUtils.createFromSpec(arg));
+        }
+        Collections.sort(versions);
+        for (VersionSpec version : versions) {
+            System.out.println(version.renderStandard());
+        }
     }
+
 }
