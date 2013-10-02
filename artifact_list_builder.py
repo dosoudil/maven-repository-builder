808463955bdf1675527bbf66bd9e73c747535740
Petr Kocandrle <pkocandr@redhat.com>
2013-10-02 22:53:21 +0200

    Enhance error message when running lftp find

--- artifact_list_builder.py
+++ artifact_list_builder.py
@@ -560,7 +560,7 @@ class ArtifactListBuilder:
                      + ' ; find  ."', stdout=PIPE, shell=True)
         result = lftp.communicate()[0]
         if lftp.returncode:
-            raise IOError("lftp execution ended by return code %d", lftp.returncode)
+            raise IOError("lftp find in %s ended by return code %d" % (url, lftp.returncode))
         else:
             return result
 
