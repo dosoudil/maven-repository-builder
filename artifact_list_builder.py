4f0a4a6708d364df46eea774dbcbe441495aab11
Paul Gier <pgier@redhat.com>
2015-08-10 14:50:18 -0500

    Fix #82: Improve error handling in lftp execution
    If lftp is not able to change to the appropriate directory, it should immediately exit.

--- artifact_list_builder.py
+++ artifact_list_builder.py
@@ -724,7 +724,7 @@ class ArtifactListBuilder:
     def _lftpFind(self, url):
         if maven_repo_util.urlExists(url):
             lftp = Popen(r'lftp -c "set ssl:verify-certificate no ; open ' + url
-                         + ' ; find  ."', stdout=PIPE, shell=True)
+                         + ' || exit 1; find  ."', stdout=PIPE, shell=True)
             result = lftp.communicate()[0]
             if lftp.returncode:
                 raise IOError("lftp find in %s ended by return code %d" % (url, lftp.returncode))
